13a2518e09b82adbd52ce73b47e35102f849819a
kinotonik <zedmed@kinotonik.fr>
2023-02-15 15:40:08 +0100

    Card de Dirk le chevalier avec interpolation et binding property

---
+++ src/app/card/User.ts
@@ -0,0 +1,14 @@
+export class User {
+  name:string;
+  age:number;
+  quote:string;
+  photo:string;
+
+  constructor(name:string, age:number,quote:string,photo:string){
+    this.name = name;
+    this.age =age;
+    this.quote = quote;
+    this.photo = photo;
+  }
+}
+
